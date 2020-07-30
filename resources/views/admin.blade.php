f29e45ff8fa492d96f5bc0e16995c29dde3dfe09
Shakil Alam <itxshakil@gmail.com>
2020-07-30 12:24:02 +0530

    Added Authentication for Admin

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    @if (Auth::guard('web')->check())
+                    <h4 class="text-lg text-center text-success">You are logged in as User!</h4>
+                    @else
+                    <h4 class="text-lg text-center text-danger">You are logged out as User!</h4>
+                    @endif
+                    @if (Auth::guard('admin')->check())
+                    <h4 class="text-lg text-center text-success">You are logged in as Admin!</h4>
+                    @else
+                    <h4 class="text-lg text-center text-danger">You are logged out as Admin!</h4>
+                    @endifF
+
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,23 @@ Route::get('/', function () {
 Auth::routes(['register' => false]);
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::namespace('Auth\Admin')->group(function () {
+        Route::get('/login', 'LoginController@showLoginForm')->name('login');
+        Route::post('/login', 'LoginController@login')->name('login.submit');
+        Route::get('/register', 'LoginController@showRegistrationForm')->name('register');
+        Route::post('/register', 'LoginController@register')->name('register.submit');
+        Route::post('/logout', 'LoginController@logout')->name('logout');
+
+        // Password reset routes
+
+        Route::post('/password/email', 'ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+        Route::post('/password/reset', 'ResetPasswordController@reset')->name('password.update');
+        Route::get('/password/reset', 'ForgotPasswordController@showLinkRequestForm')->name('password.request');
+        Route::get('/password/reset/{token}', 'ResetPasswordController@showResetForm')->name('password.reset');
+    });
+
+    Route::get('/', 'AdminController@index')->name('admin');
+});
