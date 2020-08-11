d8eb521990a605e1919c37e46eefff8c5c596727
Shakil Alam <itxshakil@gmail.com>
2020-08-11 18:48:41 +0530

    Added imageable relationships

--- app/User.php
+++ app/User.php
@@ -77,4 +77,9 @@ class User extends Authenticatable
 
         return $ratings->contains($book->id);
     }
+
+    public function image()
+    {
+        return $this->morphOne(Image::class, 'imageable');
+    }
 }
