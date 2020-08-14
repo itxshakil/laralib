95f0b82a9a4f304b3fed1a1468c79be2f6763d10
Shakil Alam <itxshakil@gmail.com>
2020-08-14 11:04:10 +0530

    Chnage authors introduction column type to text from string

--- database/migrations/2020_07_28_071751_create_authors_table.php
+++ database/migrations/2020_07_28_071751_create_authors_table.php
@@ -16,7 +16,7 @@ class CreateAuthorsTable extends Migration
         Schema::create('authors', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('introduction')->nullable();
+            $table->text('introduction')->nullable();
             $table->string('email')->nullable();
             $table->timestamps();
         });
