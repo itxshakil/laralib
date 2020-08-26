f5a741a43b06caa3d9030ea09864f39195177037
Shakil Alam <itxshakil@gmail.com>
2020-08-26 18:57:29 +0530

    Added temporary email address

--- resources/views/auth/admin/login.blade.php
+++ resources/views/auth/admin/login.blade.php
@@ -16,7 +16,7 @@
                     <input
                         class="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none @error('email') border-red-500 @enderror"
                         type="email" id="email" name="email" autocomplete="email" placeholder="john.doe"
-                        value="oarya@example.org" required autofocus />
+                        value="{{App\Admin::first()->email}}" required autofocus />
                     @error('email')
                     <p class="text-xs italic text-red-500" role="alert">{{ $message }}</p>
                     @enderror
