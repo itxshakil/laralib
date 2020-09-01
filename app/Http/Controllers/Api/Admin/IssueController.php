d17f46d3ebd851a30cf573874d26b6665a4c2eb8
Shakil Alam <itxshakil@gmail.com>
2020-09-01 20:49:47 +0530

    remove unused methods from api admin issueController

--- app/Http/Controllers/Api/Admin/IssueController.php
+++ app/Http/Controllers/Api/Admin/IssueController.php
@@ -57,35 +57,4 @@ class IssueController extends Controller
 
         return response($issue, 201);
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Book                 $book
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Book $book)
-    {
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Book                 $book
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Book $book)
-    {
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Book                 $book
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Book $book)
-    {
-    }
 }
