21a58facec53aa4caeb10aa0fab5bf18ce8a829e
Shakil Alam <itxshakil@gmail.com>
2020-09-01 20:43:06 +0530

    remove unused methids from api admin authorcontroller

--- app/Http/Controllers/Api/Admin/AuthorController.php
+++ app/Http/Controllers/Api/Admin/AuthorController.php
@@ -2,11 +2,9 @@
 
 namespace App\Http\Controllers\Api\Admin;
 
-use App\Book;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\AuthorResource;
 use App\Author;
-use Illuminate\Http\Request;
 
 class AuthorController extends Controller
 {
@@ -18,46 +16,5 @@ class AuthorController extends Controller
     public function index()
     {
         return AuthorResource::collection(Author::paginate(20));
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Book                 $book
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Book $book)
-    {
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Book                 $book
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Book $book)
-    {
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Book                 $book
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Book $book)
-    {
     }
 }
