16467d9ff84b7cd62e411a935f968a0a5810728a
Shakil Alam <itxshakil@gmail.com>
2020-09-01 20:32:59 +0530

    remove unused methods from Api Admin usercontroller

--- app/Http/Controllers/Api/Admin/UserController.php
+++ app/Http/Controllers/Api/Admin/UserController.php
@@ -31,46 +31,6 @@ class UserController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Book                 $book
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Book $book)
-    {
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Book                 $book
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Book $book)
-    {
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Book                 $book
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Book $book)
-    {
-    }
-    /**
      * Remove the specified resource from storage.
      *
      * @param  \App\Book                 $book
