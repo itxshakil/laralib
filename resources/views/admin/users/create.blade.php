021043e139da99e2d9f9cddb814503fb31071242
Shakil Alam <itxshakil@gmail.com>
2020-09-02 20:36:16 +0530

    updated user create form

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -1,23 +1,25 @@
 @extends('layouts.admin.app')
 
 @section('content')
-<div class="max-w-7xl mx-auto px-6">
+<div class="container mx-auto">
     <div class="flex justify-between items-center">
         <h3 class="text-2xl p-2">{{ __('Create User') }}</h3>
         <a href="{{route('admin.users.index')}}"
-            class="align-baseline py-2 px-4 border border-transparent text-sm  font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700">All Students</a>
+            class="align-baseline py-2 px-4 border border-transparent text-sm  font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700">All
+            Students</a>
     </div>
-    <div class="w-full bg-gray-200 p-2 md:p-5 rounded-lg lg:rounded-l-none">
-        <h3 class="pt-4 text-2xl text-center pb-2 md:pb-4">Add New Student!</h3>
-        <form class="px-4 md:px-8  pt-6 pb-2 mb-4 bg-white rounded" method="POST"
+    <div class="w-full bg-gray-200 p-2 md:p-5 shadow rounded-lg">
+        <h3 class="py-4 text-2xl text-center">Add New Student!</h3>
+        <form class="px-4 md:px-8  pt-6 pb-2 mb-4 bg-white rounded shadow" method="POST"
             action="{{route('admin.users.store')}}">
             <div class="flex flex-col sm:flex-row gap-4">
                 <section class="sm:mb-4 w-full">
                     <label class="block mb-2 text-sm font-bold text-gray-700" for="name">
                         Name
                     </label>
-                    <input type="text" name="name" id="name" value="{{old('name')}}" placeholder="John Doe"
-                        class="w-full py-2 px-4 border text-sm  font-medium rounded-md focus:outline-none focus:border-blue-700 focus:shadow-outline-blue active:bg-blue-700">
+                    <input type="text" name="name" id="name" value="{{old('name')}}"
+                        class="w-full py-2 px-4 border text-sm  font-medium rounded-md focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700"
+                        placeholder="John Doe" required autofocus>
                     @error('name')
                     <p class="text-xs italic text-red-500" role="alert">{{ $message }}</p>
                     @enderror
@@ -26,8 +28,9 @@
                     <label class="block mb-2 text-sm font-bold text-gray-700" for="email">
                         Email Address
                     </label>
-                    <input type="email" name="email" id="email" value="{{old('email')}}" placeholder="john@example.com"
-                        class="w-full py-2 px-4 border text-sm  font-medium rounded-md focus:outline-none focus:border-blue-700 focus:shadow-outline-blue active:bg-blue-700">
+                    <input type="email" name="email" id="email" value="{{old('email')}}"
+                        class="w-full py-2 px-4 border text-sm  font-medium rounded-md focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700"
+                        placeholder="john@example.com" required>
                     @error('email')
                     <p class="text-xs italic text-red-500" role="alert">{{ $message }}</p>
                     @enderror
@@ -39,7 +42,8 @@
                         Class
                     </label>
                     <input type="text" name="class" id="class" value="{{old('class')}}" placeholder="BCA 1st"
-                        class="w-full py-2 px-4 border text-sm  font-medium rounded-md focus:outline-none focus:border-blue-700 focus:shadow-outline-blue active:bg-blue-700">
+                        class="w-full py-2 px-4 border text-sm  font-medium rounded-md focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700"
+                        required>
                     @error('class')
                     <p class="text-xs italic text-red-500" role="alert">{{ $message }}</p>
                     @enderror
@@ -48,8 +52,9 @@
                     <label class="block mb-2 text-sm font-bold text-gray-700" for="rollno">
                         Roll Number
                     </label>
-                    <input type="number" name="rollno" id="rollno" value="{{old('rollno')}}" placeholder="327166363"
-                        class="w-full py-2 px-4 border text-sm  font-medium rounded-md focus:outline-none focus:border-blue-700 focus:shadow-outline-blue active:bg-blue-700">
+                    <input type="number" name="rollno" id="rollno" value="{{old('rollno')}}"
+                        class="w-full py-2 px-4 border text-sm  font-medium rounded-md focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700"
+                        placeholder="327166363" required>
                     @error('rollno')
                     <p class="text-xs italic text-red-500" role="alert">{{ $message }}</p>
                     @enderror
@@ -58,9 +63,9 @@
                     <label class="block mb-2 text-sm font-bold text-gray-700" for="password">
                         Student Password
                     </label>
-                    <input type="text" name="password" id="password" value="{{old('password')}}" placeholder="password"
-                        value="password"
-                        class="w-full py-2 px-4 border text-sm  font-medium rounded-md focus:outline-none focus:border-blue-700 focus:shadow-outline-blue active:bg-blue-700">
+                    <input type="text" name="password" id="password" value="{{old('password') ?? "password"}}"
+                        class="w-full py-2 px-4 border text-sm  font-medium rounded-md focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700"
+                        placeholder="password" required>
                     @error('password')
                     <p class="text-xs italic text-red-500" role="alert">{{ $message }}</p>
                     @enderror
@@ -68,7 +73,7 @@
             </div>
             <section class="mb-4 text-center">
                 <button
-                    class="w-full bg-blue-500 active:bg-blue-800 text-white px-3 sm:px-4 py-2 rounded-full outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md font-bold text-xs"
+                    class="w-full bg-indigo-500 active:bg-indigo-800 text-white px-3 sm:px-4 py-2 rounded-full outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md font-bold text-xs"
                     type="submit">
                     Save
                 </button>
