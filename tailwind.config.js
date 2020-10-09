3c35a44414a39adfe749ab60108ce787a454c62b
Shakil Alam <itxshakil@gmail.com>
2020-10-09 09:33:23 +0530

    whitelist tailwind navbar class list

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,7 @@ module.exports = {
     ],
     options: {
       // defaultExtractor: (content) => content.match(/[\w-/.:]+(?<!:)/g) || [],
+      whitelist: ['hidden' ,'sm:flex-1', 'sm:flex', 'sm:items-center', 'sm:justify-between'],
       whitelistPatterns: [/-active$/, /-enter$/, /-leave-to$/, /show$/],
     }
   },
