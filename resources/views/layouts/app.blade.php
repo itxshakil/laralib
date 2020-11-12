ca84de37d1b21592c5a315a9b7641d4250dba574
Shakil Alam <itxshakil@gmail.com>
2020-11-12 18:00:36 +0530

    Added additional meta tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,26 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="google-site-verification" content="oRXIYL6yenkxhE2G4UhEjBQarn6Sb3Z3jRtBdBCoAWA" />
+    <meta name="description"
+        content="Laralib is a Library Management System with Admin and User Authentication system, where you can manage users (students), books, author, and issue logs. It also has an automatic fine calculation for the late returned book." />
+    <meta name="keywords"
+        content="library management system,laravel library management system,laravel library management system with admin panel,laravel,admin panel,laravel project,laravel 8,library management system in laravel" />
+    <meta name="subject" content="Laravel Library Mangement | Laralib" />
+    <meta name="language" content="en" />
+    <meta name="rating" content="General" />
+    <meta name="url" content="https://laralib.herokuapp.com" />
+    <meta name="identifier-URL" content="https://laralib.herokuapp.com" />
+    <meta name="owner" content="Shakil Alam" />
+    <meta name="author" content="Shakil Alam , itxshakil@gmail.com" />
+    <meta name="og:title" content="Portfolio" />
+    <meta name="og:url" content="https://laralib.herokuapp.com" />
+    <meta name="og:url" content="https://laralib.herokuapp.com" />
+    <meta name="og:url" content="https://laralib.herokuapp.com/" />
+    <meta name="og:site_name" content="Shakil Alam | Portfolio" />
+    <meta name="og:description"
+        content="Laralib is a Library Management System with Admin and User Authentication system, where you can manage users (students), books, author, and issue logs. It also has an automatic fine calculation for the late returned book." />
+    <meta name="og:email" content="itxshakil@gamil.com" />
+    <meta name="og:country-name" content="India" />
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
