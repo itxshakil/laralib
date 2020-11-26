597ef22689ba6c06eb502f4e7bb1690f01d9eed2
Shakil Alam <itxshakil@gmail.com>
2020-11-26 17:13:06 +0530

    Some UI Changes in Book View and Index Page

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -10,15 +10,16 @@
     <div class="flex flex-col sm:flex-row flex-wrap flex-stretch gap-4 justify-center">
         @foreach($books as $book)
         <div class="w-full sm:w-1/3 lg:w-1/4 xl:w-1/5 bg-gray-800 text-gray-100 rounded p-4 shadow flex-grow">
-            <a href="{{route('books.show',$book)}}" class="text-xl">{{$book->title}}</a>
-            <p class="mt-2">Written By @foreach($book->authors as $author) <a
-                    href="{{route('authors.show',$author)}}">{{$author->name}}@if ($loop->remaining),
+            <a href="{{route('books.show',$book)}}" class="text-2xl font-semibold">{{$book->title}}</a>
+            <p class="mt-2 font-semibold">Written By @foreach($book->authors as $author) <a
+                    href="{{route('authors.show',$author)}}" class="font-normal">{{$author->name}}@if
+                    ($loop->remaining),
                     @endif </a>@endforeach</p>
             <span
                 class="text-sm capitalize inline-block items-center px-2 rounded-full {{$book->count ? 'bg-green-800 text-green-100' : 'bg-red-800 text-red-100'}}">{{$book->count ? 'Available' : 'Not Available'}}</span>
             <span class="text-gray-400 text-sm capitalize">in {{$book->language}}</span>
             <x-book-average-rating average-rating="{{$book->average_rating}}" />
-            <p class="text-gray-400 text-sm">ISBN : {{$book->isbn}}</p>
+            <p class="text-gray-400 text-sm mt-4">ISBN : {{$book->isbn}}</p>
         </div>
         @endforeach
     </div>
