90243b255ee9f316cbfd0c4b1312ee9c3b3e11e8
Shakil Alam <itxshakil@gmail.com>
2020-12-17 20:15:29 +0530

    Extract Method for issueBookToAuthUser

--- tests/Feature/RatingTest.php
+++ tests/Feature/RatingTest.php
@@ -30,14 +30,7 @@ class RatingTest extends TestCase
      */
     public function user_can_rate_only_issued_book()
     {
-        $book = Book::factory()->create();
-        $user = User::factory()->create([
-            'course_id' => Course::factory()->create()->id
-        ]);
-
-        $this->actingAs($user);
-
-        $book->issue_logs()->create(['user_id' => $user->id, 'admin_id' => Admin::factory()->create()->id]);
+        list($book, $user) = $this->issueBookToAuthUser();
 
         $this->post("/ratings/{$book->id}", ['score' => 1, 'message' => "123", 'user_id' => $user->id, 'book_id' => $book->id])->assertRedirect("/books/".$book->id);
     }
@@ -47,6 +40,17 @@ class RatingTest extends TestCase
      */
     public function user_can_add_only_one_rating_for_books()
     {
+        list($book, $user) = $this->issueBookToAuthUser();
+
+        $this->post("/ratings/{$book->id}", ['score' => 1, 'message' => "123", 'user_id' => $user->id, 'book_id' => $book->id])->assertRedirect("/books/".$book->id);
+        $this->post("/ratings/{$book->id}", ['score' => 1, 'message' => "123", 'user_id' => $user->id, 'book_id' => $book->id])->assertStatus(403);
+    }
+
+    /**
+     * @return array
+     */
+    protected function issueBookToAuthUser(): array
+    {
         $book = Book::factory()->create();
         $user = User::factory()->create([
             'course_id' => Course::factory()->create()->id
@@ -55,8 +59,6 @@ class RatingTest extends TestCase
         $this->actingAs($user);
 
         $book->issue_logs()->create(['user_id' => $user->id, 'admin_id' => Admin::factory()->create()->id]);
-
-        $this->post("/ratings/{$book->id}", ['score' => 1, 'message' => "123", 'user_id' => $user->id, 'book_id' => $book->id])->assertRedirect("/books/".$book->id);
-        $this->post("/ratings/{$book->id}", ['score' => 1, 'message' => "123", 'user_id' => $user->id, 'book_id' => $book->id])->assertStatus(403);
+        return array($book, $user);
     }
 }
